fdd1838fbac7fbe81e600c68a67b213bf0d522c9
Shallyslack <shallymishra698@gmail.com>
2025-04-05 16:55:18 +0530

    fixed bugs

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
 from chatapp import create_app, Socketio
+import socket
+
+def get_private_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # Attempt to connect to an external server to determine the local IP
+        s.connect(('10.254.254.254', 1))  # This IP address is just a dummy address
+        private_ip = s.getsockname()[0]
+    except Exception:
+        private_ip = '127.0.0.1'  # Fallback to localhost if the connection fails
+    finally:
+        s.close()
+    return private_ip
+
 
 app = create_app()
 
 
-Socketio.run(app, host="192.168.41.188", port=5000) 
+Socketio.run(app, host= f"{get_private_ip()}", port=5000) 
